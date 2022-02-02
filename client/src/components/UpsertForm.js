0b7404a142cf3f78918110d999e6461c0369432f
Mudit Mathur <mudit.manu.mathur@gmail.com>
2022-02-02 17:58:28 +0530

    loadin state added

--- client/src/components/UpsertForm.js
+++ client/src/components/UpsertForm.js
@@ -4,12 +4,13 @@ import {create,update} from '../helper/api.helper.js'
 const UpsertForm = ({selected,setSelected,setData,data}) => {
 
     const [content,setContent] = useState("");
+    const [loading,setLoading] =useState(false);
 
     //Async Funtion to call create/Update method and update the state
     const Upsert =async (e)=>{
         e.preventDefault();
        // console.log(content);
-
+        setLoading(true);
         if(selected){
             let response=await update(selected._id,content);
             if(response)
@@ -31,6 +32,8 @@ const UpsertForm = ({selected,setSelected,setData,data}) => {
            setData([response.data,...data]);
            //console.log(response);
         }
+        setContent("");
+        setLoading(false);
 
     }
 
@@ -47,16 +50,17 @@ const UpsertForm = ({selected,setSelected,setData,data}) => {
       <form onSubmit={(e)=>Upsert(e)}>
           <div className='mb-5'>
           <input
-            className='p-2 rounded-l outline-none border w-80 '
+            className='p-2 rounded-l outline-none border md:w-80 w-56 '
             required placeholder='Enter Your Thought Here'
             onChange={e=>setContent(e.target.value)}
             value={content}
           />
           <button
-           className=' bg-slate-900 text-white py-2 px-3 rounded-r-lg '
+            disabled={loading}
+           className=' bg-slate-900 text-white py-2 px-3 rounded-r-lg disabled:bg-slate-300 '
            type='submit' >{!selected?"Add":"Update"}</button>
            {selected && <button
-            className='text-red-500 ml-2 underline'
+            className='text-red-500 ml-2 underline '
            type='Cancel' onClick={()=>{
                setSelected(null)
                setContent("")
--- client/src/helper/api.helper.js
+++ client/src/helper/api.helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:""
+    baseURL:process.env.Node_ENV==="production"?"/":"http://localhost:5005"
 })
 
 
